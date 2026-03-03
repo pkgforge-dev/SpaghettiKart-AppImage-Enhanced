166b970844c73faae85ef5fbbd842c83fb9557ab
Link4Electronics <link4electronics@gmail.com>
2026-03-03 00:03:29 -0300

    Update make-appimage.sh
    Update make-appimage.sh
    
    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,16 +5,17 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/spaghettikart.png
+export ICON=https://raw.githubusercontent.com/HarbourMasters/SpaghettiKart/refs/heads/main/icon.png
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/* /usr/bin/zenity
-echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
