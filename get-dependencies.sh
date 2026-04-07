db9fdf8af96322f4ea2d454d06678d261e7367c2
Link4Electronics <link4electronics@gmail.com>
2026-04-07 04:55:20 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,23 +9,18 @@ echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     cmake         \
     fmt           \
-    libdecor      \
     libzip        \
-    ninja         \
     nlohmann-json \
-    sdl2          \
     sdl2_net      \
     spdlog        \
     tinyxml2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
 make-aur-package zenity-rs-bin
 
-# If the application needs to be manually built that has to be done down here
 echo "Making stable build of SpaghettiKart..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/HarbourMasters/SpaghettiKart"
@@ -36,17 +31,9 @@ echo "$VERSION" > ~/version
 mkdir -p ./AppDir/bin
 cd ./SpaghettiKart
 patch -Np1 -i ../spaghettikart-cmake-flags.patch
-cmake . \
-    -Bbuild \
-    -GNinja \
-    -DNON_PORTABLE=On
-cmake --build build --config Release
-cmake --build build --config Release --target GenerateO2R
+cmake ./ -Bbuild -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config Release -j$(nproc)
+cmake --build build --config Release --target GenerateO2R -j$(nproc)
 
-mv -v build/yamls ../AppDir/bin
-mv -v build/meta ../AppDir/bin
-mv -v build/Spaghettify ../AppDir/bin
-mv -v build/config.yml ../AppDir/bin
-mv -v build/spaghetti.o2r ../AppDir/bin
+mv -v build/yamls build/meta build/Spaghettify build/config.yml build/spaghetti.o2r ../AppDir/bin
 wget -O ../AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
-cp -rv icon.png /usr/share/pixmaps/spaghettikart.png
