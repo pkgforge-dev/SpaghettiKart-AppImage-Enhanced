1d5380d738786eae0acfc280f951dcfa5bdc890e
Link4Electronics <link4electronics@gmail.com>
2026-08-21 06:36:04 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,6 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/* /usr/bin/zenity
-#echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
