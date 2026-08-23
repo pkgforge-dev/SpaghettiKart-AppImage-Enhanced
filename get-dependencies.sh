fc0a45dd70c40df7cf179fbf4f55aad90054bafe
Link4Electronics <link4electronics@gmail.com>
2026-08-23 17:55:56 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -46,5 +46,3 @@ mv -v build/Spaghettify ../AppDir/bin
 mv -v build/config.yml ../AppDir/bin
 mv -v build/spaghetti.o2r ../AppDir/bin
 wget -O ../AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
-cp -v icon.png ../AppDir/.DirIcon
-mv -v icon.png ../AppDir/spaghettikart.png
